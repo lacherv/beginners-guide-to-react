a21241370eac7f67b58a3f1bfe06f48915138929
Christian Awelakoue <awelakoue@gmail.com>
2021-07-21 19:57:06 +0200

    create a ui with vanilla js and dom

---
+++ main.js
@@ -0,0 +1,5 @@
+const rootElement = document.getElementById('root');
+const element = document.createElement('div');
+element.textContent = 'Hello lacherv';
+element.className = 'containter';
+rootElement.appendChild(element);
